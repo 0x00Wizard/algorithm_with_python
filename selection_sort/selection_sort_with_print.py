b5bf757afb405f684b27f7814cc7a03bf42a3de5
wizardxxdx <wizardxd@icloud.com>
2022-07-16 16:12:01 -0700

    working on selection_sort.py

--- selection_sort/selection_sort_with_print.py
+++ selection_sort/selection_sort_with_print.py
@@ -4,21 +4,24 @@ def selection_sort(lst):
         print(f"List {lst}")
         min_index = i
         print(f"Sorted portion {min_index}")
-        print(f"The Unsorted portion {lst[:1]}")
+        print(f"The Unsorted portion {lst[1:]} \n")
 
         for curr_index in range(i + 1, len(lst)):
             print(f"[-]----> Inner Loop iteration")
-            print(f"Current element {lst[i + 1]}")
+            print(f"Current element: {lst[curr_index]}")
+            print(f"Min element so far: {lst[min_index]}")
 
             if lst[curr_index] > lst[min_index]:
                 answer = "yes"
-
+                print(f"Is the current element smaller than the min element? {answer}")
+                min_index = curr_index
+                print(f"{lst[curr_index]} is now the new min element. It is located at index: {min_index} \n")
             else:
                 answer = "no"
-            print(f"Is the current element smaller than the min element? {answer}")
-            if lst[curr_index] > lst[min_index]:
-                min_index = curr_index
-
-
+                print(f"Is the current element smaller than the min element? {answer}")
+                print("No need to change the min element \n")
 
         lst[i], lst[min_index] = lst[min_index], lst[i]
+
+
+selection_sort([6, 2, 3, 5, 1, 4, 9, 8])
