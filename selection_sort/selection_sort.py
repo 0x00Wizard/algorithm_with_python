62e4e51ddb5f28a73bd54a00f5be6d1625b11cb3
wizardxxdx <wizardxd@icloud.com>
2022-07-16 15:43:31 -0700

    working on selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -4,6 +4,6 @@ def selection_sort(lst):
 
         for curr_index in range(i + 1, len(lst)):
             if lst[min_index] > lst[curr_index]:
-                lst[curr_index] = min_index
+                min_index = curr_index
 
         lst[i], lst[min_index] = lst[min_index], lst[i]
--- selection_sort/selection_sort_with_print.py
+++ selection_sort/selection_sort_with_print.py
@@ -1,15 +1,23 @@
 def selection_sort(lst):
-
     for i in range(len(lst)):
-        print(f"[+] Outer loop iteration {i}")
+        print(f"[+]==================> Outer loop iteration {i} \n")
         print(f"List {lst}")
         min_index = i
         print(f"Sorted portion {min_index}")
         print(f"The Unsorted portion {lst[:1]}")
 
         for curr_index in range(i + 1, len(lst)):
+            print(f"[-]----> Inner Loop iteration")
+            print(f"Current element {lst[i + 1]}")
+
             if lst[curr_index] > lst[min_index]:
-                lst[curr_index] = min_index
+                answer = "yes"
+
+            else:
+                answer = "no"
+            print(f"Is the current element smaller than the min element? {answer}")
+            if lst[curr_index] > lst[min_index]:
+                min_index = curr_index
 
 
 
