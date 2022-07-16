6ad8193f6dc1ff29f4bd07ffef60e0cf15349b9a
wizardxxdx <wizardxd@icloud.com>
2022-07-16 15:08:22 -0700

    working on selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,1 +1,9 @@
-def selection(sort)
+def selection_sort(lst):
+    for i in range(len(lst)):
+        min_index = i
+
+        for curr_index in range(i + 1, len(lst)):
+            if lst[min_index] > lst[curr_index]:
+                lst[curr_index] = min_index
+
+        lst[i], lst[min_index] = lst[min_index], lst[i]
