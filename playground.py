752ac36d084d04987ade827a020a0057e5650b3b
wizardxxdx <wizardxd@icloud.com>
2022-07-18 07:19:59 -0700

    working on selection_sort.py

--- playground.py
+++ playground.py
@@ -1,29 +1,10 @@
-def bubble_sort(lst):
-    print(f"List {lst}")
-    n = len(lst)
-    print(f"Length of list {n}")
-    for i in range(n):
-        print("=" * 50)
-        print(f"index of the outer-loop {i} \n")
-        swapped = False
+def selection_sort(lst):
+    for i in range(len(lst)):
+        min_index = i
 
-        for j in range(0, n - i - 1):
-            calculation = n - i - 1
-            print(f"{n} - {i} - 1 = {calculation}")
-            print(f"index of the inner loop {j} \n")
-            print(f"List {lst}")
-            print(f"first index {j} = {lst[j]} \n")
-            print(f"Second index {j + 1} = {lst[j + 1]} \n")
-
-            if lst[j] > lst[j + 1]:
-                lst[j], lst[j + 1] = lst[j + 1], lst[j]
-                print(f"list {lst} \n")
-                print(f"Yes {lst[j]} > {lst[j + 1]} \n")
-
-                swapped = True
-
-        if not swapped:
-            break
+        for curr_index in range(i + 1, len(lst)):
+            if lst[min_index] > lst[curr_index]:
+                min_index = curr_index
 
 
-print(bubble_sort([6, 2, 3, 5, 1, 4, 9, 8]))
+        lst[i], lst[min_index] = lst[min_index], lst[i]
