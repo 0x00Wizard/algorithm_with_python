f49ab2d3896897a4b86453cc8f2219d7caee2975
wizardxxdx <wizardxd@icloud.com>
2022-07-23 16:23:54 -0700

    working on insertion_sort.py

--- playground.py
+++ playground.py
@@ -1,9 +1,11 @@
 def insertion_sort(lst):
-    for i in range(len(lst)):
+    for i in range(1, len(lst)):
         elem_selected = lst[i]
 
         while i > 0 and elem_selected < lst[i - 1]:
-            lst[i] = lst[i - 1]
+            lst[i] = lst[i-1]
             i -= 1
+
+
 
         lst[i] = elem_selected
