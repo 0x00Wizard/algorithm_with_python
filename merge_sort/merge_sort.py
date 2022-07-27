7874e21357b0ab060da72dea8d69025056141736
wizardxxdx <wizardxd@icloud.com>
2022-07-27 09:11:09 -0700

    working on insertion_sort.py

--- merge_sort/merge_sort.py
+++ merge_sort/merge_sort.py
@@ -1,3 +1,26 @@
+def merge(left_half, right_half):
+    if not left_half or not right_half:
+        return left_half or right_half
+
+    result = []
+    i, j = 0,0
+
+    while True:
+
+        if left_half[i] < right_half[j]:
+            result.append(left_half[i])
+            i += 1
+        else:
+            result.append(right_half[i])
+            j += 1
+
+        if i == len(left_half) or j == len(right_half):
+            result.extend(left_half[i:] or right_half[j:])
+            break
+
+    return result
+
+
 def merge_sort(lst):
     if len(lst) == 0 or len(lst) == 1:
         return lst
