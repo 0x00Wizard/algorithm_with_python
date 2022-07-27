60368507952fe53f55eebae76cc7b40d3c5da8eb
wizardxxdx <wizardxd@icloud.com>
2022-07-27 09:04:49 -0700

    working on insertion_sort.py

--- merge_sort/merge_sort.py
+++ merge_sort/merge_sort.py
@@ -1,1 +1,11 @@
-def merge_sort
+def merge_sort(lst):
+    if len(lst) == 0 or len(lst) == 1:
+        return lst
+
+    else:
+        middle_index = len(lst)//2
+
+        left = merge_sort(lst[:middle_index])
+        right = merge_sort(lst[middle_index:])
+
+        return merge(left, right)
