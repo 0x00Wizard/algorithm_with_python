a63c41a5b2ce900d7f734422aae389f50e653fae
wizardxxdx <wizardxd@icloud.com>
2022-07-27 08:49:04 -0700

    working on insertion_sort.py

--- playground.py
+++ playground.py
@@ -1,18 +1,51 @@
-def insertion_sort(lst):
-    print(f"The list {lst}")
-    print(f"The Unsorted list {lst[1:]}")
+# Write your code below this line
+# Define a fuction name sort_client
+# def sort_client(lst):
+#     for i in range(1, len(lst)):
+#         elem_selected = lst[i]
+#
+#         while i > 0 and elem_selected < lst[i - 1]:
+#             lst[i] = lst[i - 1]
+#             i -= 1
+#
+#         lst[i] = elem_selected
+#
+# Define a sort_clients function that takes a list as a parameter.
+#
+# The list contains tuples. The structure of each tuple is: (<client_name>, <current_balance>) where client_name is a
+# string and current_balance is an integer (for this exercise, you can assume that the balances are integers).
+#
+# Example: [("Nora", 20000), ("Gino", 500), ("Danny", 152), ("William", 10000)]
+#
+# The function must implement the Insertion Sort algorithm to sort the clients in ascending order, according to their
+# current balance.
+#
+# Your function must return a tuple with the following elements (in this exact order):
+#
+#     The number of assignments performed (number of iterations of the for loop).
+#
+#     The number of iterations of the inner (while) loop.
+#
+#     The name of the client with the greatest balance.
+
+
+def sort_client(lst):
+    num_swaps = 0
+    num_iter_inner_loop = 0
+
     for i in range(1, len(lst)):
         elem_selected = lst[i]
-        print(f"Element Selected: {elem_selected}")
-        print(f"The swap {lst[i]} = {lst[i - 1]}")
-        while i > 0 and elem_selected < lst[i - 1]:
+
+        while i > 0 and elem_selected[1] < lst[i - 1][1]:
+            num_iter_inner_loop += 1
             lst[i] = lst[i - 1]
-            # print(f"The swap {lst[i]} = {lst[i - 1]}")
-            i -= 1
 
         lst[i] = elem_selected
+        num_swaps += 1
+
+    return (num_swaps, num_iter_inner_loop, lst[-1][0])
 
 
+a = [("Nora", 20000), ("Gino", 500), ("Danny", 152), ("William", 10000)]
 
-
-insertion_sort([1, 6, 8, 2, 6])
+print(sort_client(a))
