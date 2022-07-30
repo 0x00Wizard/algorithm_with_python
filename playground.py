b66bff2d3ede4ab9e9029994897b9e3465087292
wizardxxdx <wizardxd@icloud.com>
2022-07-30 15:44:02 -0700

    working on merge_sort.py

--- playground.py
+++ playground.py
@@ -6,16 +6,19 @@ def merge(left_half, right_half):
     i, j = 0, 0
 
     while True:
+
         if left_half[i] < right_half[j]:
             result.append(left_half[i])
             i += 1
-
         else:
             result.append(right_half[j])
             j += 1
 
         if i == len(left_half) or j == len(right_half):
-            result.extend(left_half[i:] or right_half[j:])
+            result.extend(left_half[i:] or right_half[:j])
+            break
+
+    return result
 
 
 def merge_sort(lst):
@@ -23,8 +26,8 @@ def merge_sort(lst):
         return lst
 
     else:
-        middle_index = len(lst) // 2
+        middle_index = len(lst)//2
         left = merge_sort(lst[:middle_index])
-        right = merge_sort(lst[:middle_index])
+        right = merge_sort(lst[middle_index:])
 
         return merge(left, right)
