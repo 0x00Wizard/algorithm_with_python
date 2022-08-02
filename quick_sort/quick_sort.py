876c930b95e306ae7af39d5dcad7fe6e7bb97940
wizardxxdx <wizardxd@icloud.com>
2022-08-01 18:58:10 -0700

    working on merge_sort.py

--- quick_sort/quick_sort.py
+++ quick_sort/quick_sort.py
@@ -1,6 +1,10 @@
+def partition(i, j, k):
+    pass
+
+
 def quicksort(lst, low, high):
     if low < high:
         pivot_index = partition(lst, low, high)
 
         quicksort(lst, low, pivot_index-1)
-        quicksort(lst, pivot_index+1, high)
+        quicksort(lst, pivot_index + 1, high)
