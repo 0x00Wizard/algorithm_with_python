bb5924cdd3f06961f78c8278be582bfad7047820
wizardxxdx <wizardxd@icloud.com>
2022-08-05 08:43:05 -0700

    working on merge_sort.py

--- playground.py
+++ playground.py
@@ -1,33 +1,19 @@
-def merge(left_half, right_half):
-    if not left_half or not right_half:
-        return left_half or right_half
+def partition(lst, low, high):
+    pivot = lst[high]
+    i = low - 1
 
-    result = []
-    i, j = 0, 0
-
-    while True:
-        if left_half[i] < right_half[j]:
-            result.append(left_half[i])
+    for j in range(low, high):
+        if lst[j] <= pivot:
             i += 1
+            lst[i], lst[j] = lst[j], lst[i]
 
-        else:
-            result.append(right_half[j])
-            j += 1
-
-        if i == len(left_half) or j == len(right_half[j]):
-            result.extend(left_half[i:] or right_half[:j])
-            break
-
-    return result
+    lst[i + 1], lst[high] = lst[high], lst[i + 1]
+    return i + 1
 
 
-def merge_sort(lst):
-    if len(lst) == 0 or len(lst) == 1:
-        return lst
+def quick_sort(lst, low, high):
+    if low < high:
+        pivot_index = partition(lst, low, high)
 
-    else:
-        middle_index = len(lst) // 2
-        left = merge_sort(lst[:middle_index])
-        right = merge_sort(lst[middle_index:])
-
-        merge(left, right)
+        quick_sort(lst, low, pivot_index - 1)
+        quick_sort(lst, pivot_index + 1, high)
