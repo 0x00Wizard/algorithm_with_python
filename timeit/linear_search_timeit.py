4cc55de765ad6fc4c93e87e2b86be7d2db7ad0f7
wizardxxdx <wizardxd@icloud.com>
2022-08-07 16:20:16 -0700

    working on linear_search_timeit.py timeit

--- timeit/linear_search_timeit.py
+++ timeit/linear_search_timeit.py
@@ -2,16 +2,23 @@ import timeit
 
 NUM_OF_REPETITION = 1000
 
+
 # Linear Search Algorithm Implementation
 
 def linear_search(lst, item):
     for i in range(len(lst)):
-        if lst[i] == item
+        if lst[i] == item:
             return i
     return -1
+
 
 # list
 
 a = [i for i in range(10000)]
 
-#
+# First Test - Finding an element at the beginning of the list
+
+test_code_1 = '''
+linear_search(a, 50)
+'''
+
