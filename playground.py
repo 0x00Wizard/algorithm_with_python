afe259251eb5dd65b6d7222e2fdba80c13ac56ea
wizardxxdx <wizardxd@icloud.com>
2022-08-07 16:18:13 -0700

    working on merge_sort.py

--- playground.py
+++ playground.py
@@ -1,28 +1,5 @@
-import random
+import timeit
 
-a = [i for i in range(1000)]
-
-random.shuffle(a)
+print(timeit.timeit("[i for i in range(10000)]", number=500))
 
 
-def partition(lst, low, high):
-    pivot = lst[high]
-
-    i = low - 1
-
-    for j in range(low, high):
-        if lst[j] <= pivot:
-            i += 1
-            lst[i], lst[j] = lst[j], lst[i]
-
-    lst[i + 1], lst[high] = lst[i + 1], lst[high]
-    return i + 1
-
-
-def quicksort(lst, low, high):
-
-    if low < high:
-        pivot_index = partition(lst, low, high)
-
-        partition(lst, low, pivot_index - 1)
-        partition(lst, pivot_index - 1, high)
---
+++ timeit/linear_search_timeit.py
@@ -0,0 +1,17 @@
+import timeit
+
+NUM_OF_REPETITION = 1000
+
+# Linear Search Algorithm Implementation
+
+def linear_search(lst, item):
+    for i in range(len(lst)):
+        if lst[i] == item
+            return i
+    return -1
+
+# list
+
+a = [i for i in range(10000)]
+
+#
