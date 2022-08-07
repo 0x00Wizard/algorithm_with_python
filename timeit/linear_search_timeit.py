3eac92ff004cfe16d2a5d2620b9a03ef03d7527d
wizardxxdx <wizardxd@icloud.com>
2022-08-07 16:31:39 -0700

    working on linear_search_timeit.py timeit

--- timeit/linear_search_timeit.py
+++ timeit/linear_search_timeit.py
@@ -1,6 +1,6 @@
 import timeit
 
-NUM_OF_REPETITION = 1000
+NUM_REPETITIONS = 1000
 
 
 # Linear Search Algorithm Implementation
@@ -12,13 +12,50 @@ def linear_search(lst, item):
     return -1
 
 
-# list
-
+# List
 a = [i for i in range(10000)]
 
 # First Test - Finding an element at the beginning of the list
 
 test_code_1 = '''
+
 linear_search(a, 50)
+
 '''
 
+print("\n==> First test of Linear Search: Beginning")
+
+time = timeit.timeit(test_code_1, number=NUM_REPETITIONS, globals=globals())
+
+print("Total time to find the element:", time)
+print("Average time per repetition:", time / NUM_REPETITIONS)
+
+# Second Test - Finding an element in the middle of the list
+
+test_code_2 = '''
+
+linear_search(a, 4500)
+
+'''
+
+print("\n==> Second test of Linear Search: Middle")
+
+time = timeit.timeit(test_code_2, number=NUM_REPETITIONS, globals=globals())
+
+print("Total time to find the element:", time)
+print("Average time per repetition:", time / NUM_REPETITIONS)
+
+# Third Test - Finding an element at the end of the list
+
+test_code_3 = '''
+
+linear_search(a, 9999)
+
+'''
+
+print("\n==> Third test of Linear Search: End")
+
+time = timeit.timeit(test_code_3, number=NUM_REPETITIONS, globals=globals())
+
+print("Total time to find the element:", time)
+print("Average time per repetition:", time / NUM_REPETITIONS)
